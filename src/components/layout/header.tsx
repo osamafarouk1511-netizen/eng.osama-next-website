c9696813cbddfc67f706d53ee71f0f4bee5a74b1
Ahmedsalah39622 <239156109+osamafarouk1511-netizen@users.noreply.github.com>
2025-12-02 15:16:07 +0200

    fixing 5

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion'
 import { Menu, X } from 'lucide-react'
 import Link from 'next/link'
+import Image from 'next/image'
 import { useState } from 'react'
 import { Button } from '../ui/button'
 
